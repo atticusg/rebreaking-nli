aad5c2f1de0967cadfed8a68278c4e361a9a3d20
atticusg <atticusg@gmail.com>
2019-11-10 01:13:13 -0800

    break og

---
+++ totsv.py
@@ -0,0 +1,14 @@
+import csv
+import json
+out = "meme.tsv"
+name = "break.jsonl"
+with open(out, "wt") as g:
+    tsv = csv.writer(g, delimiter="\t")
+    with open(name, "r") as f:
+        index = 0
+        labels = ['index', 'promptID', 'pairID', 'genre', 'sentence1_binary_parse', 'sentence2_binary_parse', 'sentence1_parse', 'sentence2_parse', 'sentence1', 'sentence2', 'label1', 'label2', 'label3', 'label4', 'label5', 'gold_label']
+        tsv.writerow(labels)
+        for line in f.readlines():
+            example = json.loads(line.strip())
+            tsv.writerow([index, "a", "b", "c", "d","e","f","g","h",example["sentence1"], example["sentence2"], "a", "b", "c", "d", "e", example["gold_label"]])
+            index += 1
